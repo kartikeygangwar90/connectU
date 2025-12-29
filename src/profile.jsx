7763252974fe9217ceeeffa7ee6fb0d9b68406d4
Kartikey Gangwar <kartikeygangwar90@gmail.com>
2025-12-30 00:54:55 +0530

    working on new profile.jsx file

--- src/profile.jsx
+++ src/profile.jsx
@@ -79,27 +79,27 @@
 //                 <div className="container page">
 //             <h2 className='heading'>Step - 1 : Basic Identity</h2>
 //             <div className="card1">
-//                 <h4 id='fhname'>First Name : </h4>
-//                 <input type="text" value={userData.firstName} placeholder='First name here' id='fname' onChange={(e) =>
+//                 <h4 className='fhname'>First Name : </h4>
+//                 <input type="text" value={userData.firstName} placeholder='First name here' className='fname' onChange={(e) =>
 //                     setUserData({ ...userData, firstName: e.target.value })
 //                 }
 //                 />
-//                 <h4 id='lhname'>Last Name : </h4>
-//                 <input type="text" id="lname" placeholder='Last name here' value={userData.lastName} onChange={(e) =>
+//                 <h4 className='lhname'>Last Name : </h4>
+//                 <input type="text" className="lname" placeholder='Last name here' value={userData.lastName} onChange={(e) =>
 //                     setUserData({ ...userData, lastName: e.target.value })
 //                 } />
-//                 <h4 id='gender'>Gender : </h4>
-//                 <input type="radio" name="gender" id="male-inp" className='male' value="Male" checked={userData.gender === "Male"} onChange={(e) =>
+//                 <h4 className='gender'>Gender : </h4>
+//                 <input type="radio" name="gender" className="male-inp" className='male' value="Male" checked={userData.gender === "Male"} onChange={(e) =>
 //                     setUserData({ ...userData, gender: e.target.value })
-//                 }></input><h4 className='male' id='Male'>Male</h4>
-//                 <input type="radio" name="gender" id="female-inp" className='female' value="Female" checked={userData.gender === "Female"} onChange={(e) =>
+//                 }></input><h4 className='male' className='Male'>Male</h4>
+//                 <input type="radio" name="gender" className="female-inp" className='female' value="Female" checked={userData.gender === "Female"} onChange={(e) =>
 //                     setUserData({ ...userData, gender: e.target.value })
-//                 } /><h4 className='female' id='Female'>Female</h4>
-//                 <input type="radio" name="gender" id="other-inp" className='other' value="Other" checked={userData.gender === "Other"} onChange={(e) =>
+//                 } /><h4 className='female' className='Female'>Female</h4>
+//                 <input type="radio" name="gender" className="other-inp" className='other' value="Other" checked={userData.gender === "Other"} onChange={(e) =>
 //                     setUserData({ ...userData, gender: e.target.value })
-//                 } /><h4 className='other' id='Other'>Other</h4>
-//                 <h4 id='roll-head'>Roll number : </h4>
-//                 <input type="text" inputMode='numeric' pattern='[0-9]' id="roll" placeholder='Enter Roll number ' value={userData.roll} onChange={(e) =>
+//                 } /><h4 className='other' className='Other'>Other</h4>
+//                 <h4 className='roll-head'>Roll number : </h4>
+//                 <input type="text" inputMode='numeric' pattern='[0-9]' className="roll" placeholder='Enter Roll number ' value={userData.roll} onChange={(e) =>
 //                     setUserData({ ...userData, roll: e.target.value })
 //                 } />
 //             </div>
@@ -206,8 +206,8 @@ function ProfileSetup() {
 
     return (
         <main>
-            <div className="complete--page">
-                <h2 id="page--heading">Create Your Profile</h2>
+            <div className="complete--page--p">
+                <h2 className="page--heading">Create Your Profile</h2>
                 <p>Join the community and find your perfect team</p>
 
                 <div className="boxes">
@@ -238,36 +238,36 @@ function ProfileSetup() {
                     </div>
                 </div>
 
-                {step === 1 && <div className="info--section">
+                {step === 1 && <div className="info--section--p">
                     <h3 class="head-tag">Personal Info</h3>
                     <p class="tag">Lets start with the basics</p>
                     <h5>Full Name <span className="required">*</span></h5>
                     <input
                         type="text"
-                        id="full--name"
+                        className="full--name"
                         placeholder="Enter your full name"
                         value={userData.value}
                         onChange={(e) => setUserData({ ...userData, fullName: e.target.value })}
                     />
-                    <h5 id="email--heading">Email Address <span className="required">*</span></h5>
+                    <h5 className="email--heading">Email Address <span className="required">*</span></h5>
                     <input
                         type="text"
-                        id="email--input"
+                        className="email--input"
                         placeholder="your.email@gmail.com"
                         value={userData.value}
                         onChange={(e) => setUserData({ ...userData, email: e.target.value })}
                     />
-                    <h5 id="college--input">University <span className="required">*</span></h5>
+                    <h5 className="college--input">University <span className="required">*</span></h5>
                     <select
-                        id='options--college'
+                        className='options--college'
                         value={userData.college}
                         onChange={(e) => setUserData({ ...userData, college: e.target.value })
                         }>
                         <option value="" disabled>Select Your College</option>
-                        <option value="NITP" id='college-selection'>national Institute of Technology Patna</option>
+                        <option value="NITP" className='college-selection'>national Institute of Technology Patna</option>
                     </select>
                     <h5>Year <span className="required">*</span></h5>
-                    <select id="options--year" value={userData.year} onChange={(e) =>
+                    <select className="options--year" value={userData.year} onChange={(e) =>
                         setUserData({ ...userData, year: e.target.value })
                     }>
                         <option value="" disabled>Select Current Year</option>
@@ -277,7 +277,7 @@ function ProfileSetup() {
                         <option value="4">4th Year</option>
                     </select>
                     <h5>Major/Field of Study <span className="required">*</span></h5>
-                    <select id='options--branch' value={userData.branch} onChange={(e) =>
+                    <select className='options--branch' value={userData.branch} onChange={(e) =>
                         setUserData({ ...userData, branch: e.target.value })
                     }>
                         <option value="" disabled>Select Your Branch</option>
@@ -285,10 +285,10 @@ function ProfileSetup() {
                         <option value="ECE">Electronics and Communication</option>
                         <option value="MNC">Mathematics and Computing</option>
                     </select>
-                    <h5 id="bio--heading">Bio </h5>
+                    <h5 className="bio--heading">Bio </h5>
                     <input
                         type="text"
-                        id="bio--input"
+                        className="bio--input"
                         placeholder="Tell us a bit about yourself... (optional)"
                         value={userData.value}
                         onChange={(e) => setUserData({ ...userData, description: e.target.value })}
@@ -297,10 +297,10 @@ function ProfileSetup() {
                     <button id="nextt" className={`next ${step === 4 ? "preview-active" : ""}`} onClick={step === 4 ? Preview : nextStep} >{step === 4 ? "Preview" : "next"}</button>
                 </div>
                 }
-                {step === 2 && <div className="skills--info">
+                {step === 2 && <div className="skills--info--p">
                     <h3 class="head-tag skills">Your Skills</h3>
                     <p class="tag skills">Let others discover what you bring to a team</p>
-                    <h5 id="technical--heading">Technical Skills <span className="required">*</span></h5>
+                    <h5 className="technical--heading">Technical Skills <span className="required">*</span></h5>
                     <p className="tag">Add skills you can Confidently contribute</p>
                     <button className="suggestion" onClick={() => addSkills("React JS")}>React JS &gt;</button>
                     <button className="suggestion" onClick={() => addSkills("Node.Js")}>Node.js &gt;</button>
@@ -308,12 +308,28 @@ function ProfileSetup() {
                     <button className="suggestion" onClick={() => addSkills("UI/UX Designer")}>Machine Learning &gt;</button>
                     <button className="suggestion" onClick={() => addSkills("UI/UX Designer")}>Marketing &gt;</button>
                     <button className="suggestion" onClick={() => addSkills("UI/UX Designer")}>Robotics &gt;</button>
-                    <input
-                     type="text"
-                       id="skill--input"
-                       placeholder="Add custom skill ..." 
-
-                       />
+                    <div className="skill--input--area">
+                        <input
+                            type="text"
+                            className="skill--input"
+                            placeholder="Add custom skill ..."
+                            value={skillInput}
+                            onChange={(e) => setSkillInput(e.target.value)}
+                            onKeyDown={(e) => {
+                                if (e.key === "Enter") {
+                                    e.preventDefault();
+                                    addSkills(skillInput);
+                                    setSkillInput("");
+                                }
+                            }}
+                        />
+                        <button className="add--skill" >Add</button>
+                    </div>
+                    <div className="skill-choosen">
+                        {skills.map((skill, index) => (
+                            <h3 className="skills" key={index}>{skill}</h3>
+                        ))}
+                    </div>
                 </div>
 
                 }

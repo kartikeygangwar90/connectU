d379cf40589dbf1e31fcfe72eb160941f77c8c61
Kartikey Gangwar <kartikeygangwar90@gmail.com>
2025-12-30 16:48:29 +0530

    step - 2 Ready

--- src/profile.jsx
+++ src/profile.jsx
@@ -180,8 +180,10 @@ function ProfileSetup() {
         course: "",
         branch: "",
         year: "",
-        skills: [],
+        technicalSkills: [],
+        softSkills: [],
         interests: [],
+        experience: "",
         email: "",
         phone: "",
         availability: ""
@@ -189,19 +191,47 @@ function ProfileSetup() {
 
     const [skillInput, setSkillInput] = React.useState("");
     const [skills, setSkills] = React.useState([]);
-
-    function addSkills(skill) {
+    const [clicked, setClicked] = React.useState([]);
+    function addSkills(skill, val) {
+        setClicked([...clicked, val]);
         if (!skill.trim()) return;
         if (skills.includes(skill)) return;
-
+        
         setSkills([...skills, skill]);
+        
+        const selected = userData.technicalSkills.includes(skill);
+        
+        setUserData({
+            ...userData,
+            technicalSkills: selected ? userData.technicalSkills.filter(i => i !== skill) : [...userData.technicalSkills, skill]
+        })
+        setSkillInput("")
+    }
+    
+    const [softSkillInput, setSoftSkillInput] = React.useState("");
+    const [softSkills, setSoftSkills] = React.useState([]);
+    const [softClicked, setSoftClicked] = React.useState([])
+    function addSoftSkills(skill, val) {
+        setSoftClicked([...softClicked, val])
+        if (!skill.trim()) return;
+        if (softSkills.includes(skill)) return;
 
-        const selected = userData.skills.includes(skill);
+        setSoftSkills([...softSkills, skill]);
+
+        const selected = userData.softSkills.includes(skill);
 
         setUserData({
             ...userData,
-            skills: selected ? userData.skills.filter(i => i !== skill) : [...userData.skills, skill]
+            softSkills: selected ? userData.softSkills.filter(i => i !== skill) : [...userData.softSkills, skill]
         })
+        setSoftSkillInput("")
+    }
+    const [experience, setExperience] = React.useState("")
+    const [expClicked, setExpClicked] = React.useState(0)
+    function addExperience (exp, value) {
+        if (!exp.trim()) return;
+        setExperience(exp)
+        setExpClicked(value)
     }
 
     return (
@@ -214,34 +244,34 @@ function ProfileSetup() {
                     <div className="box">
                         <div className="circle">1</div>
                         <h4>Personal Info</h4>
-                        <p>Tell us about yourself</p>
+                        <p className="render">Tell us about yourself</p>
                     </div>
                     <div className="box">
                         <div className="circle">2</div>
                         <h4>Skills</h4>
-                        <p>what are you good at ?</p>
+                        <p className="render">what are you good at ?</p>
                     </div>
                     <div className="box">
                         <div className="circle">3</div>
                         <h4>Interests</h4>
-                        <p>What drives you ?</p>
+                        <p className="render">What drives you ?</p>
                     </div>
                     <div className="box">
                         <div className="circle">4</div>
                         <h4>Contact Info</h4>
-                        <p>How can we reach you ?</p>
+                        <p className="render">How can we reach you ?</p>
                     </div>
                     <div className="box">
                         <div className="circle">5</div>
                         <h4>Review</h4>
-                        <p>Almost done</p>
+                        <p className="render">Almost done</p>
                     </div>
                 </div>
 
                 {step === 1 && <div className="info--section--p">
                     <h3 class="head-tag">Personal Info</h3>
                     <p class="tag">Lets start with the basics</p>
-                    <h5>Full Name <span className="required">*</span></h5>
+                    <h5 className="name--heading">Full Name <span className="required">*</span></h5>
                     <input
                         type="text"
                         className="full--name"
@@ -266,7 +296,7 @@ function ProfileSetup() {
                         <option value="" disabled>Select Your College</option>
                         <option value="NITP" className='college-selection'>national Institute of Technology Patna</option>
                     </select>
-                    <h5>Year <span className="required">*</span></h5>
+                    <h5 className="year--heading">Year <span className="required">*</span></h5>
                     <select className="options--year" value={userData.year} onChange={(e) =>
                         setUserData({ ...userData, year: e.target.value })
                     }>
@@ -276,7 +306,7 @@ function ProfileSetup() {
                         <option value="3">3rd Year</option>
                         <option value="4">4th Year</option>
                     </select>
-                    <h5>Major/Field of Study <span className="required">*</span></h5>
+                    <h5 className="branch--heading">Major/Field of Study <span className="required">*</span></h5>
                     <select className='options--branch' value={userData.branch} onChange={(e) =>
                         setUserData({ ...userData, branch: e.target.value })
                     }>
@@ -300,19 +330,19 @@ function ProfileSetup() {
                 {step === 2 && <div className="skills--info--p">
                     <h3 class="head-tag skills">Your Skills</h3>
                     <p class="tag skills">Let others discover what you bring to a team</p>
-                    <h5 className="technical--heading">Technical Skills <span className="required">*</span></h5>
+                    <h4 className="technical--heading">Technical Skills <span className="required">*</span></h4>
                     <p className="tag">Add skills you can Confidently contribute</p>
-                    <button className="suggestion" onClick={() => addSkills("React JS")}>React JS &gt;</button>
-                    <button className="suggestion" onClick={() => addSkills("Node.Js")}>Node.js &gt;</button>
-                    <button className="suggestion" onClick={() => addSkills("UI/UX Designer")}>UI/UX Designer &gt;</button>
-                    <button className="suggestion" onClick={() => addSkills("UI/UX Designer")}>Machine Learning &gt;</button>
-                    <button className="suggestion" onClick={() => addSkills("UI/UX Designer")}>Marketing &gt;</button>
-                    <button className="suggestion" onClick={() => addSkills("UI/UX Designer")}>Robotics &gt;</button>
+                    <button className={`suggestion ${ clicked.includes(1) ? "suggestion--clicked" : ""}`} onClick={() => addSkills("React JS", 1)}>React JS &gt;</button>
+                    <button className={`suggestion ${ clicked.includes(2) ? "suggestion--clicked" : ""}`} onClick={() => addSkills("Node.Js", 2) }>Node.js &gt;</button>
+                    <button className={`suggestion ${ clicked.includes(3) ? "suggestion--clicked" : ""}`} onClick={() => addSkills("UI/UX Designer", 3)}>UI/UX Designer &gt;</button>
+                    <button className={`suggestion ${ clicked.includes(4) ? "suggestion--clicked" : ""}`} onClick={() => addSkills("Machine learning", 4)}>Machine Learning &gt;</button>
+                    <button className={`suggestion ${ clicked.includes(5) ? "suggestion--clicked" : ""}`} onClick={() => addSkills("Marketing", 5)}>Marketing &gt;</button>
+                    <button className={`suggestion ${ clicked.includes(6) ? "suggestion--clicked" : ""}`} onClick={() => addSkills("Robotics", 6)}>Robotics &gt;</button>
                     <div className="skill--input--area">
                         <input
                             type="text"
                             className="skill--input"
-                            placeholder="Add custom skill ..."
+                            placeholder="Add custom Technical skills ..."
                             value={skillInput}
                             onChange={(e) => setSkillInput(e.target.value)}
                             onKeyDown={(e) => {
@@ -323,13 +353,51 @@ function ProfileSetup() {
                                 }
                             }}
                         />
-                        <button className="add--skill" >Add</button>
+                        <button className="add--skill" onClick={() => addSkills(skillInput)}>Add</button>
                     </div>
                     <div className="skill-choosen">
                         {skills.map((skill, index) => (
                             <h3 className="skills" key={index}>{skill}</h3>
                         ))}
                     </div>
+                    <h4 className="technical--heading">Soft Skills <span className="required">*</span></h4>
+                    <p className="tag">Talent that you have</p>
+                    <button className={`suggestion--softskill ${softClicked.includes(1) ? "suggestion--clicked" : ""}`} onClick={() => addSoftSkills("Leadership", 1)}>Leadership &gt;</button>
+                    <button className={`suggestion--softskill ${softClicked.includes(2) ? "suggestion--clicked" : ""}`} onClick={() => addSoftSkills("Problem solving", 2)}>Problem solving &gt;</button>
+                    <button className={`suggestion--softskill ${softClicked.includes(3) ? "suggestion--clicked" : ""}`} onClick={() => addSoftSkills("Team work", 3)}>Team work &gt;</button>
+                    <button className={`suggestion--softskill ${softClicked.includes(4) ? "suggestion--clicked" : ""}`} onClick={() => addSoftSkills("Critical Thinking", 4)}>Critical Thinking &gt;</button>
+                    <div className="softSkill--input--area">
+                    <input
+                        type="text"
+                        className="softSkill--input"
+                        placeholder="Add custom Soft skills ..."
+                        value={softSkillInput}
+                        onChange={(e) => setSoftSkillInput(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                e.preventDefault();
+                                addSoftSkills(softSkillInput);
+                                setSoftSkillInput("");
+                            }
+                        }}
+                    />
+                    <button className="add--skill" onClick={() => addSoftSkills(softSkillInput)}>Add</button>
+                    </div>
+                    <div className="skill-choosen">
+                        {softSkills.map((skill, index) => (
+                            <h3 className="skills" key={index}>{skill}</h3>
+                        ))}
+                    </div>
+                    <h4 className="technical--heading">Experience Level <span className="required">*</span></h4>
+                    <div className="exp--boxes">
+                        <button className={`exp--box ${expClicked === 1 ? "clicked--exp" : ""}`} onClick={() => addExperience("Beginner", 1)}>Beginner</button>
+                        <button className={`exp--box ${expClicked === 2 ? "clicked--exp" : ""}`} onClick={() => addExperience("Intermediate", 2)}>Intermediate</button>
+                        <button className={`exp--box ${expClicked === 3 ? "clicked--exp" : ""}`} onClick={() => addExperience("Advanced", 3)}>Advanced</button>
+                        <button className={`exp--box ${expClicked === 4 ? "clicked--exp" : ""}`} onClick={() => addExperience("Expert", 4)}>Expert</button>
+                    </div>
+                    <button className='previous' onClick={prevStep} disabled={step === 1} >Previous</button>
+                    <button className={`next ${step === 4 ? "preview-active" : ""}`} onClick={step === 4 ? Preview : nextStep} >{step === 4 ? "Preview" : "next"}</button>
+
                 </div>
 
                 }
